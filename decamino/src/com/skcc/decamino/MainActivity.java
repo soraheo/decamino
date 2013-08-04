12ca08c6ed3463355954944028f8f1c418070b22
tj0822 <tj820822@gmail.com>
2013-08-04 08:34:38 -0430

    232

--- decamino/src/com/skcc/decamino/MainActivity.java
+++ decamino/src/com/skcc/decamino/MainActivity.java
@@ -17,8 +17,6 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-		//Å×½ºÆ®1234 
-		
 	}
 
 }
